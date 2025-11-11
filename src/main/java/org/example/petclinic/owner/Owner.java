29f6f8c28acab78f4fa2eb028a202eb94b4bda41
JoeMThomas <143606608+JoeMThomas@users.noreply.github.com>
2025-11-11 16:42:19 +0000

    feat: add variables and methods to Owner

--- src/main/java/org/example/petclinic/owner/Owner.java
+++ src/main/java/org/example/petclinic/owner/Owner.java
@@ -1,6 +1,35 @@
 package org.example.petclinic.owner;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Owner {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private long id;
+
+    private String firstName;
+    private String lastName;
+
+    private String email;
+    private String phone;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
 }
